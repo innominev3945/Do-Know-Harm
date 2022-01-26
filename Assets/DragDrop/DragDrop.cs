356fd01ef5b694c04bb0495d1ee9d7fe29ad8424
gabo458 <81783303+gabo458@users.noreply.github.com>
2022-01-25 22:19:52 -0800

    ui / mouse drag on tool testing
    followed some tutorials on making a healthbar and drag/drop interaction (can be used with tools for wounds?)
    No assets so rectangles work lol

---
+++ Assets/DragDrop/DragDrop.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler, IDropHandler
+{
+    // see https://www.youtube.com/watch?v=BGr-7GZJNXg
+    // note that this entire script is only for UI, need collider and OnMouseDrag for sprites
+    private RectTransform rectTransform;
+    private CanvasGroup canvasGroup;
+
+    [SerializeField] private Canvas canvas;
+
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        canvasGroup = GetComponent<CanvasGroup>();
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("OnPointerDown");
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        // Debug.Log("OnDrag");
+        rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnBeginDrag");
+        canvasGroup.alpha = 0.6f;
+        canvasGroup.blocksRaycasts = false;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnEnddrag");
+        canvasGroup.alpha = 1f;
+        canvasGroup.blocksRaycasts = true;
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+}
