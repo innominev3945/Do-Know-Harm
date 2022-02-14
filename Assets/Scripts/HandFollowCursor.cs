a5d4615969299587980680bf1038d3c67bb8be61
juuli43 <julia.endriga@gmail.com>
2022-02-13 22:46:41 -0800

    makeshift hand "tool" follows cursor

--- Assets/Scripts/HandFollowCursor.cs
+++ Assets/Scripts/HandFollowCursor.cs
@@ -7,6 +7,7 @@ public class HandFollowCursor : MonoBehaviour
     void Update()
     {
         Vector2 mousePosition = Mouse.current.position.ReadValue();
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
         transform.position = new Vector2(mousePosition.x, mousePosition.y);
     }
 }
