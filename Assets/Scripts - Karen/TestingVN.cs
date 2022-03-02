542dba8d9c2b660e9ea0ffd40b3d6744fd517522
bluepaw59 <bluepaw59@gmail.com>
2022-03-01 22:34:53 -0800

    comments to document save

--- Assets/Scripts - Karen/TestingVN.cs
+++ Assets/Scripts - Karen/TestingVN.cs
@@ -5,12 +5,25 @@ using UnityEngine;
 public class TestingVN : MonoBehaviour
 {
     DialogueManager dialoguer;
+    private int saveIndex = 0;
+    private int saveChapter = 0;
 
     [SerializeField] private TextAsset[] txtAssets;
     void Start()
     {
         dialoguer = GetComponent<DialogueManager>();
+
+        //Starts the VN Scene corresponding to the text asset at [0]
         dialoguer.StartScene(txtAssets[0]);
+
+        // gets the index in chapter where the player is currently at
+        saveIndex = dialoguer.getSaveLocation(); 
+        //when save, save the text asset number(chapter number)
+
+        //load a save VN Scene by doing the following:
+        //dialoguer.loadVNScene(savedIndex, txtAssets[saveChapter]);
+
+
     }
 
     
