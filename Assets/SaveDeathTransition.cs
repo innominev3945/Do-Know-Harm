6e7f0860677cf85e1d04cc4d30e92d1067d1b3e4
gabo458 <81783303+gabo458@users.noreply.github.com>
2022-05-15 19:34:00 -0700

    minor stuff

--- Assets/SaveDeathTransition.cs
+++ Assets/SaveDeathTransition.cs
@@ -16,7 +16,7 @@ public class SaveDeathTransition : MonoBehaviour
     [SerializeField] private GameObject saveText;
     [SerializeField] private GameObject deathText;
     [SerializeField] private GameObject backgroundFade;
-
+    [SerializeField] private GameObject fadePanel;
 
     // Start is called before the first frame update
     void Start()
@@ -33,7 +33,61 @@ public class SaveDeathTransition : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (moving)
+        /*if (moving)
+        {
+            Debug.Log(moving);
+            //float posTempX = Mathf.Lerp(transform.position.x, zoomPoint.x, Time.deltaTime * 5);
+            *//*float newY = Mathf.Pow((startY - transform.position.y), 2) / 300;
+            
+            transform.position = new Vector3(transform.position.x, transform.position.y - Mathf.Max(newY, 0.001f), transform.position.z);
+            stretcher.transform.position = new Vector3(stretcher.transform.position.x, stretcher.transform.position.y - Mathf.Max(newY, 0.001f), transform.position.z);
+            
+
+            *//*
+            Vector3 tempVel = velocity;
+            transform.position = Vector3.SmoothDamp(transform.position, target, ref velocity, 1f);
+            stretcher.transform.position = Vector3.SmoothDamp(stretcher.transform.position, target, ref tempVel, 1f);
+            if (!patientSaved)
+            {
+                eyeMask.transform.position = Vector3.SmoothDamp(eyeMask.transform.position, target, ref tempVel, 1f);
+            }
+            if (transform.position.y < -10f)
+            {
+                moving = false;
+            }
+        }*/
+    }
+
+    public void SaveTransition()
+    {
+        saveText.SetActive(true);
+        StartCoroutine(MoveSavedPatient());
+        //StartCoroutine(FadeTextToFullAlpha(1f, SaveText.GetComponent<TextMesh>()));
+    }
+
+    public void DeathTransition()
+    {
+        deathText.SetActive(true);
+        StartCoroutine(MoveDeadPatient());
+    }
+
+
+    public void currentPatientDeath(GameObject patient) //param - patient gameobject
+    {
+        //saveText.setActive(true);
+        StartCoroutine(MoveSavedPatient());
+    }
+
+    IEnumerator MoveSavedPatient()
+    {
+        patientSaved = true;
+        StartCoroutine(FadeTextToFullAlpha(1f, saveText.GetComponent<TextMeshProUGUI>()));
+        StartCoroutine(FadeImageToFullAlpha(1f, backgroundFade.GetComponent<Image>()));
+        yield return new WaitForSeconds(0.6f);
+        stretcher.SetActive(true);
+        yield return new WaitForSeconds(0.6f);
+        moving = true;
+        while (moving)
         {
             Debug.Log(moving);
             //float posTempX = Mathf.Lerp(transform.position.x, zoomPoint.x, Time.deltaTime * 5);
@@ -55,31 +109,8 @@ public class SaveDeathTransition : MonoBehaviour
             {
                 moving = false;
             }
+            yield return null;
         }
-    }
-
-    public void SaveTransition()
-    {
-        saveText.SetActive(true);
-        StartCoroutine(MoveSavedPatient());
-        //StartCoroutine(FadeTextToFullAlpha(1f, SaveText.GetComponent<TextMesh>()));
-    }
-
-    public void DeathTransition()
-    {
-        deathText.SetActive(true);
-        StartCoroutine(MoveDeadPatient());
-    }
-
-    IEnumerator MoveSavedPatient()
-    {
-        patientSaved = true;
-        StartCoroutine(FadeTextToFullAlpha(1f, saveText.GetComponent<TextMeshProUGUI>()));
-        StartCoroutine(FadeImageToFullAlpha(1f, backgroundFade.GetComponent<Image>()));
-        yield return new WaitForSeconds(0.6f);
-        stretcher.SetActive(true);
-        yield return new WaitForSeconds(0.6f);
-        moving = true;
     }
 
     IEnumerator MoveDeadPatient()
@@ -91,6 +122,22 @@ public class SaveDeathTransition : MonoBehaviour
         eyeMask.SetActive(true);
         yield return new WaitForSeconds(0.6f);
         moving = true;
+        while (moving)
+        {
+            Debug.Log(moving);
+            Vector3 tempVel = velocity;
+            transform.position = Vector3.SmoothDamp(transform.position, target, ref velocity, 1f);
+            stretcher.transform.position = Vector3.SmoothDamp(stretcher.transform.position, target, ref tempVel, 1f);
+            if (!patientSaved)
+            {
+                eyeMask.transform.position = Vector3.SmoothDamp(eyeMask.transform.position, target, ref tempVel, 1f);
+            }
+            if (transform.position.y < -10f)
+            {
+                moving = false;
+            }
+            yield return null;
+        }
     }
 
     public IEnumerator FadeTextToFullAlpha(float t, TextMeshProUGUI i)
@@ -135,5 +182,11 @@ public class SaveDeathTransition : MonoBehaviour
             i.color = new Color(i.color.r, i.color.g, i.color.b, i.color.a - (Time.deltaTime / t));
             yield return null;
         }
+    }
+
+    public void switchPatient()
+    {
+        StartCoroutine(FadeImageToFullAlpha(1f, fadePanel.GetComponent<Image>()));
+
     }
 }
